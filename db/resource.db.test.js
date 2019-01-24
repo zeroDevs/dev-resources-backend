62fc59560bbc3048f5ff63d38e1f48923be5fb15
Sherin Binu <sherinbinu@hotmail.com>
2019-01-24 19:30:29 +0530

    test success case for resourceHandler - readAll

--- db/resource.db.test.js
+++ db/resource.db.test.js
@@ -3,15 +3,7 @@ const assert = require('assert');
 const Resource = require('../models/resource.model');
 const resourceHandler = require('./resource.db');
 const config = require('../config');
-
-const seedResource = {
-  link: 'https://www.google.com',
-  author: {
-    id: '1234567890',
-    username: 'TestUser',
-    discriminator: '1234'
-  }
-};
+const resourceSeed = require('../seeds/resources.seed');
 
 before(done => {
   mongoose.set('useCreateIndex', true);
@@ -28,14 +20,17 @@ before(done => {
 describe('resourceHandler - create()', () => {
   it('should create a new resource in the database on valid inputs', done => {
     resourceHandler
-      .create(seedResource)
+      .create(resourceSeed[0])
       .then(response => {
         if (response.error === false) {
-          Resource.findOne({ link: seedResource.link }, (error, resource) => {
-            assert.equal(resource.link, seedResource.link);
-            assert.equal(resource.author.id, seedResource.author.id);
-            done();
-          });
+          Resource.findOne(
+            { link: resourceSeed[0].link },
+            (error, resource) => {
+              assert.equal(resource.link, resourceSeed[0].link);
+              assert.equal(resource.author.id, resourceSeed[0].author.id);
+              done();
+            }
+          );
         }
       })
       .catch(console.log);
@@ -50,8 +45,46 @@ describe('resourceHandler - read()', () => {
 
 describe('resourceHandler - readAll()', () => {
   it('should return all resources', done => {
-    done();
-  });
+    Promise.all(
+      resourceSeed.splice(0, 5).map(seed => {
+        return resourceHandler.create(seed);
+      })
+    )
+      .then(() => {
+        resourceHandler.readAll().then(response => {
+          assert.equal(response.payload.resources.length, 5);
+          assert.equal(typeof response.payload.resources[0].link, 'string');
+          assert.equal(typeof response.payload.resources[1].link, 'string');
+          assert.equal(typeof response.payload.resources[2].link, 'string');
+          assert.equal(typeof response.payload.resources[3].link, 'string');
+          assert.equal(typeof response.payload.resources[4].link, 'string');
+          assert.equal(
+            typeof response.payload.resources[0].author.id,
+            'string'
+          );
+          assert.equal(
+            typeof response.payload.resources[1].author.id,
+            'string'
+          );
+          assert.equal(
+            typeof response.payload.resources[2].author.id,
+            'string'
+          );
+          assert.equal(
+            typeof response.payload.resources[3].author.id,
+            'string'
+          );
+          assert.equal(
+            typeof response.payload.resources[4].author.id,
+            'string'
+          );
+          done();
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }).timeout(10000);
 });
 
 describe('resourceHandler - updateLink()', () => {
