46c2d392672176996de2aae804b10d8a98dedc29
oghenebrume50 <lilfreenzy@gmail.com>
2019-02-23 22:43:13 +0100

    auto-submit.js error

--- utils/auto-submit.js
+++ utils/auto-submit.js
@@ -1,4 +1,6 @@
-const getUrl = require('../utils').getUrl;
+const getUrl = require('./get-url');
+const logger = require('../logger').log;
+const dbHandler = require('../db/resource.db');
 
 module.exports = (client, message) => {
     // This function has been called after a message has been detected in the dev-resources channel
@@ -7,11 +9,75 @@ module.exports = (client, message) => {
     // The message variable, will contain the message object, 
     // Console log it to see what options are available
     // call the function on line one, using the best option you found in your console log as the param
-    // eg. const url = getURL(<message param>)
+    
+    const messageUrls = getUrl(message.content)
+    //console.log(url)
+    const channel = client.channels.get(process.env.RESOURCES_CHANNEL);
+    
 
     //Step 2 - Create an object for the Authors details
     // Using data you can get from the message object in the message variable
-    // Eg. const authorObj = { id: <userID>, username: <username>, discriminator: <discriminator>, avatar: <avatar-url> }
+    if (messageUrls) {
+        const authorObj = { 
+            id: message.author.id, 
+            username: message.author.username,
+            discriminator: message.author.discriminator, 
+            avatar: message.author.avatarURL 
+        }
+
+        //console.log(channel)
+        Promise.all(
+            messageUrls.map(url => {
+            logger({
+                author: message.author.username,
+                type: 'Automatic Link Submission',
+                url: url,
+                status: 'URL Processed'
+            });
+            console.log(url, authorObj);
+            return dbHandler.create({ link: url, author: authorObj });
+            })
+        )
+        .then(responses => {
+            responses.forEach(response => {
+                channel.send({
+                    embed: {
+                    color: 4647373,
+                    title: response.payload.title,
+                    url: response.payload.url,
+                    description: "Thank you for submitting this resource, I am strong because of you 💪",
+                    thumbnail: {
+                        url: response.payload.image
+                    },
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    }
+                    }
+                });
+                logger({
+                    author: message.author.username,
+                    type: 'Automatic Link Submission',
+                    url: response.payload.url,
+                    status: 'Success',
+                    avatar: message.author.avatarURL,
+                    message: response.message
+                });
+                });
+            })
+            .catch(error => {
+                logger({
+                author: message.author.username,
+                type: 'Automatic Link Submission',
+                url: error.payload.url,
+                status: 'Error',
+                message: error.message,
+                avatar: message.author.avatarURL
+                });
+                console.log(error.message)
+            });
+    }
+    
 
     // Step 3 - Submit the URL to the database
     // Passing the url variable you created in step 1 and the user object created in step 2
