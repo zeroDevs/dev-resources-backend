91a21df5f0b799ddab7afbd4c94094c95d83087a
Sherin Binu <sherinbinu@hotmail.com>
2019-02-23 09:31:23 +0530

    add utility class Response

---
+++ utils/classes/Response.js
@@ -0,0 +1,19 @@
+class Response {
+  constructor() {
+    this.error = true;
+    this.message = 'Something went wrong. Please try again later';
+    this.payload = {};
+  }
+
+  setSuccess() {
+    this.error = false;
+  }
+
+  setMessage(message) {
+    this.message = message;
+  }
+
+  setPayload(payload) {
+    this.payload = { ...this.payload, ...payload };
+  }
+}
