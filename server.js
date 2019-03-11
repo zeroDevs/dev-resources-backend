bd5f6caf54196c0afd29b3663b7a663918f84bb2
Sherin Binu <sherinbinu@hotmail.com>
2019-03-12 00:55:38 +0530

    modularize server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+const passport = require('./auth/passport');
+
+const homeRoute = require('./routes/index.route');
+const userRoute = require('./routes/user.route');
+const resourceRoute = require('./routes/resource.route');
+const statseRoute = require('./routes/stats.route');
+const contribRoute = require('./routes/contributors.route');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+const scopes = ['identify', 'guilds'];
+
+app.use(
+  session({
+    secret: 'super batman',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', homeRoute);
+app.use('/user', userRoute);
+app.use('/resource', resourceRoute);
+app.use('/stats', statseRoute);
+app.use('/contributors', contribRoute);
+
+app.listen(port, function() {
+  console.log('Our app is running on port:' + port);
+});
